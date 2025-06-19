408cc72e9177419c22b8b0704579b514b37cfd61
0xGingi <0xgingi@0xgingi.com>
2025-06-19 10:35:25 -0400

    cleanup & bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [],
 
   // Include the public directory
   publicDir: "public",
